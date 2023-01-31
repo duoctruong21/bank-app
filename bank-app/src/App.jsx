ccd99dfc240a3e7f02651291e1e538e7fac7d593
duoctruong21 <truongduoc.it.utc2@gmail.com>
2023-01-31 21:36:52 +0700

    done

--- bank-app/src/App.jsx
+++ bank-app/src/App.jsx
@@ -1,8 +1,41 @@
+import Billing from "./components/Billing";
+import Business from "./components/Business";
+import CardDeal from "./components/CardDeal";
+import Clients from "./components/Clients";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import Stats from "./components/Stats";
+import Testimonials from "./components/Testimonials";
 import styles from "./style";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX}`}></div>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <NavBar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
   </div>
 );
 
