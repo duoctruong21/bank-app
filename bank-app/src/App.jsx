45e71e2d2839acda9dd98ecd4a9e10d98817bb00
duoctruong21 <truongduoc.it.utc2@gmail.com>
2023-01-31 14:25:19 +0700

    update

---
+++ bank-app/src/App.jsx
@@ -0,0 +1,9 @@
+import styles from "./style";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX}`}></div>
+  </div>
+);
+
+export default App;
